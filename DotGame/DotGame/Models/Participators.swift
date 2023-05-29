c630c71e6fc299ef91af93055037d832bb1a3c35
AlexHorobchuk <alexhorobchuk@gmail.com>
2023-05-29 08:41:29 -1000

    1.02

--- DotGame/DotGame/Models/Participators.swift
+++ DotGame/DotGame/Models/Participators.swift
@@ -1,37 +1,11 @@
 //
-//  Models.swift
+//  Participators.swift
 //  DotGame
 //
-//  Created by Olha Dzhyhirei on 5/28/23.
+//  Created by Olha Dzhyhirei on 5/29/23.
 //
 
 import Foundation
-
-struct Dots {
-    
-    var owner: Participator
-}
-
-class Station {
-    
-    var owner: Participator?
-    var position: (Int, Int)
-    var ballsAmount: Int
-    
-    
-    init(owner: Participator? = nil, position: (Int, Int), dotsAmount: Int) {
-        self.owner = owner
-        self.position = position
-        self.ballsAmount = dotsAmount
-    }
-    
-    func updateStation() {
-        if owner == nil && ballsAmount < 10 { ballsAmount += 1 }
-        else {
-            if ballsAmount < 50 { ballsAmount += 1 }
-        }
-    }
-}
 
 class Participator {
     
@@ -76,5 +50,3 @@ class BotPlayer: Participator {
         }
     }
 }
-
-
