75a5713404c395c14b756055a970912502055586
AlexHorobchuk <118340451+AlexHorobchuk@users.noreply.github.com>
2023-05-29 08:28:14 -1000

    Add files via upload

---
+++ DotGame/DotGame/DotGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotGameApp.swift
+//  DotGame
+//
+//  Created by Olha Dzhyhirei on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DotGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DotGame/DotGame/Protocols.swift
@@ -0,0 +1,14 @@
+//
+//  Protocols.swift
+//  DotGame
+//
+//  Created by Olha Dzhyhirei on 5/28/23.
+//
+
+import Foundation
+
+protocol MoverService: AnyObject {
+    
+    func move(ballsQountity: Int, from stationA: Station, to stationB: Station, by: Participator)
+}
+
